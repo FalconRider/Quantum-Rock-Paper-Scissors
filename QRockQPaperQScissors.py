aa2228d949c86cfe47e748b966ebcb88f5b272cb
FalconRider <pgitschner@outlook.com>
2020-10-30 09:38:50 -0400

    Update QRockQPaperQScissors.py

--- QRockQPaperQScissors.py
+++ QRockQPaperQScissors.py
@@ -43,46 +43,24 @@ S = dnx.maximum_independent_set(G, sampler=sampler, num_reads=10)
 
 # Print the solution for the user
 
+#print('Maximum independent set size found is', len(S))
 
 
-
-print('Maximum independent set size found is', len(S))
 zz = S[0]
 
-print ("value of node ************************************************* ",zz)
+#print ("value of node ************************************************* ",zz)
 
 if zz == 1:
-    print("One")
+    print("                  ROCK")
 if zz == 2:
-    print ("Two")
+    print ("                 PAPER")
 if zz == 3: 
-    print ("Three")  
+    print ("                 SCISSORS")  
 
-print("****************************************************")
+print("*************** Hit Again Arrow to Play Again *************************************")
 
-print(S)
 
-# Visualize the results
-k = G.subgraph(S)
-notS = list(set(G.nodes()) - set(S))
-othersubgraph = G.subgraph(notS)
-pos = nx.spring_layout(G)
-plt.figure()
-
-# Save original problem graph
-original_name = "antenna_plot_original.png"
-nx.draw_networkx(G, pos=pos, with_labels=True)
-plt.savefig(original_name, bbox_inches='tight')
-
-# Save solution graph
-# Note: red nodes are in the set, blue nodes are not
-solution_name = "antenna_plot_solution.png"
-nx.draw_networkx(k, pos=pos, with_labels=True, node_color='r', font_color='k')
-nx.draw_networkx(othersubgraph, pos=pos, with_labels=True, node_color='b', font_color='w')
-plt.savefig(solution_name, bbox_inches='tight')
-
-print("Your plots are saved to {} and {}".format(original_name, solution_name))
+#print(S)
 
 
 
-print(S)
